612ca7ae576e578571c9e7f12734f5e799500042
landidaniel <landi_daniel@hotmail.com>
2022-10-17 18:55:09 -0300

    Initial commit

---
+++ cliente.py
@@ -0,0 +1,13 @@
+class Cliente:
+    def __init__(self, nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        print('Conta de: ')
+        return self.__nome.title()
+
+    @nome.setter
+    def nome(self, nome):
+        print('Chamando setter nome()')
+        self.__nome = nome
